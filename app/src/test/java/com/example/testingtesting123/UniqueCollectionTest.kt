dfc5685fefc43ff31f2d1d7f3d4b889769a46a59
kieranplenn <tuk25850@temple.edu>
2024-12-04 15:15:51 -0500

    Tests check!

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -16,14 +16,27 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-
+        val item = Item("Item1")
+        collection.addItem(item)
+        assert(collection.size() == 1) { "Item was not added to the collection" }
+        assert(collection.get(0) == item) { "Incorrect item in the collection" }
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        val item1 = Item("Item1")
+        val duplicateItem = Item("Item1")
+        val uniqueItem = Item("Item2")
 
+        collection.addItem(item1)
+        collection.addItem(duplicateItem)
+        collection.addItem(uniqueItem)
+
+        assert(collection.size() == 2) { "Duplicate items were added to the collection" }
+        assert(collection.get(0) == item1) { "First item mismatch" }
+        assert(collection.get(1) == uniqueItem) { "Unique item mismatch" }
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
